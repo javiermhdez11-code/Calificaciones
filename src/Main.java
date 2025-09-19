938178f5b88ae0da2714580248e672f07a6ddf42
javiermhdez11-code <javiermhdez11@gmail.com>
2025-09-19 09:17:48 -0600

    Agregando clases JAva del proyecto

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         int [] calificaciones = {95,70, 85, 90, 90};
         String nombreEstudiante = "Javier Martínez";
 
+        //Aqui hare un comentario en esta linea
+
         Alumno alumno = new Alumno(nombreEstudiante, calificaciones);
         alumno.imprimirResultados();
     }
-
-
 }
